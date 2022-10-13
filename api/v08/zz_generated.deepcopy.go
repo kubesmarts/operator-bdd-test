desmax74/kogito-serverless-operator

[Kogito-7981] Provide a Build CRD for the kaniko Builder
Signed-off-by: desmax74 <mdessi@redhat.com>

api/v08/zz_generated.deepcopy.go
@@ -555,6 +555,125 @@ func (in *FunctionRef) DeepCopy() *FunctionRef {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *KogitoServerlessBuild) DeepCopyInto(out *KogitoServerlessBuild) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	in.Status.DeepCopyInto(&out.Status)
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new KogitoServerlessBuild.
+func (in *KogitoServerlessBuild) DeepCopy() *KogitoServerlessBuild {
+	if in == nil {
+		return nil
+	}
+	out := new(KogitoServerlessBuild)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *KogitoServerlessBuild) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *KogitoServerlessBuildList) DeepCopyInto(out *KogitoServerlessBuildList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]KogitoServerlessBuild, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new KogitoServerlessBuildList.
+func (in *KogitoServerlessBuildList) DeepCopy() *KogitoServerlessBuildList {
+	if in == nil {
+		return nil
+	}
+	out := new(KogitoServerlessBuildList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *KogitoServerlessBuildList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *KogitoServerlessBuildSpec) DeepCopyInto(out *KogitoServerlessBuildSpec) {
+	*out = *in
+	if in.SourceSwf != nil {
+		in, out := &in.SourceSwf, &out.SourceSwf
+		*out = make([]byte, len(*in))
+		copy(*out, *in)
+	}
+	if in.Dockerfile != nil {
+		in, out := &in.Dockerfile, &out.Dockerfile
+		*out = make([]byte, len(*in))
+		copy(*out, *in)
+	}
+	out.TypeMeta = in.TypeMeta
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new KogitoServerlessBuildSpec.
+func (in *KogitoServerlessBuildSpec) DeepCopy() *KogitoServerlessBuildSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(KogitoServerlessBuildSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *KogitoServerlessBuildSpec) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *KogitoServerlessBuildStatus) DeepCopyInto(out *KogitoServerlessBuildStatus) {
+	*out = *in
+	in.Deployments.DeepCopyInto(&out.Deployments)
+	in.Applied.DeepCopyInto(&out.Applied)
+	out.TypeMeta = in.TypeMeta
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new KogitoServerlessBuildStatus.
+func (in *KogitoServerlessBuildStatus) DeepCopy() *KogitoServerlessBuildStatus {
+	if in == nil {
+		return nil
+	}
+	out := new(KogitoServerlessBuildStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *KogitoServerlessBuildStatus) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *KogitoServerlessWorkflow) DeepCopyInto(out *KogitoServerlessWorkflow) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta

kubesmarts/operator-bdd-test

api/v08/zz_generated.deepcopy.go
@@ -555,6 +555,101 @@ func (in *FunctionRef) DeepCopy() *FunctionRef {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *KogitoServerlessBuild) DeepCopyInto(out *KogitoServerlessBuild) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	in.Status.DeepCopyInto(&out.Status)
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new KogitoServerlessBuild.
+func (in *KogitoServerlessBuild) DeepCopy() *KogitoServerlessBuild {
+	if in == nil {
+		return nil
+	}
+	out := new(KogitoServerlessBuild)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *KogitoServerlessBuild) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *KogitoServerlessBuildList) DeepCopyInto(out *KogitoServerlessBuildList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]KogitoServerlessBuild, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new KogitoServerlessBuildList.
+func (in *KogitoServerlessBuildList) DeepCopy() *KogitoServerlessBuildList {
+	if in == nil {
+		return nil
+	}
+	out := new(KogitoServerlessBuildList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *KogitoServerlessBuildList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *KogitoServerlessBuildSpec) DeepCopyInto(out *KogitoServerlessBuildSpec) {
+	*out = *in
+	if in.Dockerfile != nil {
+		in, out := &in.Dockerfile, &out.Dockerfile
+		*out = make([]byte, len(*in))
+		copy(*out, *in)
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new KogitoServerlessBuildSpec.
+func (in *KogitoServerlessBuildSpec) DeepCopy() *KogitoServerlessBuildSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(KogitoServerlessBuildSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *KogitoServerlessBuildStatus) DeepCopyInto(out *KogitoServerlessBuildStatus) {
+	*out = *in
+	in.Builder.DeepCopyInto(&out.Builder)
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new KogitoServerlessBuildStatus.
+func (in *KogitoServerlessBuildStatus) DeepCopy() *KogitoServerlessBuildStatus {
+	if in == nil {
+		return nil
+	}
+	out := new(KogitoServerlessBuildStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *KogitoServerlessWorkflow) DeepCopyInto(out *KogitoServerlessWorkflow) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta